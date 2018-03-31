d852f7288ac3db6c20964f2866ddee19ce037d67
Agis <agis.pof@gmail.com>
2018-03-31 23:27:09 +0100

    spacytokenizer now accepts special cases symbols when initialized

--- src/quicknlp/data/spacy_tokenizer.py
+++ src/quicknlp/data/spacy_tokenizer.py
@@ -9,13 +9,16 @@ class SpacyTokenizer:
     Spacy tokenizer can tokenizes a sentence using
     """
 
-    def __init__(self, language="en"):
+    def __init__(self, language="en", special_cases=None):
         self.nlp = spacy.load(language)
         self.nlp.tokenizer.add_special_case('<eos>', [{ORTH: '<eos>'}])
         self.nlp.tokenizer.add_special_case('<bos>', [{ORTH: '<bos>'}])
         self.nlp.tokenizer.add_special_case('<sos>', [{ORTH: '<sos>'}])
         self.nlp.tokenizer.add_special_case('<unk>', [{ORTH: '<unk>'}])
         self.nlp.tokenizer.add_special_case('<pad>', [{ORTH: '<pad>'}])
+        special_cases = [] if special_cases is None else special_cases
+        for case in special_cases:
+            self.nlp.tokenizer.add_special_case(case, [{ORTH: case}])
 
     def __call__(self, x, sentence=False):
         if sentence:
